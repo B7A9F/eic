22804ce1a87c1e32a28e5e70b692fc589f75b12e
Boris <boris.butnaru@gmail.com>
2025-12-26 02:33:42 +0200

    upd

--- next.config.js
+++ next.config.js
@@ -5,25 +5,12 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Exclude phonemize from build traces collection
+  // Exclude phonemize from build traces collection (speeds up build)
+  // But still allow it to be bundled for API routes
   experimental: {
     outputFileTracingExcludes: {
       "*": ["node_modules/phonemize/**/*"],
     },
-  },
-  // Exclude phonemize from webpack compilation completely
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Exclude phonemize from server-side compilation
-      // This prevents webpack from processing it during build
-      config.externals = config.externals || [];
-      if (Array.isArray(config.externals)) {
-        config.externals.push("phonemize");
-      } else {
-        config.externals = [config.externals, "phonemize"];
-      }
-    }
-    return config;
   },
 };
 
